1f3195fb3a4ee52613094dcf60cf304a6a05d876
Shivam Bansal <shivamsb2003@gmail.com>
2024-09-02 07:57:11 +0530

    minor ui changes

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -74,76 +74,81 @@ function Card({ item }: CardProps) {
   };
 
   return (
-    <li className="flex w-full flex-col gap-2 py-3 lg:flex-row lg:gap-4">
-      <Link
-        to={`/post/${item.id}`}
-        className="flex h-64 items-center lg:h-48 lg:w-64"
-      >
-        <img
-          src={item.image?.url.replace("upload/", "upload/c_fill,h_260,w_750/")}
-          className="h-full w-full cursor-pointer rounded-md object-cover"
-          alt="house-img"
-        />
-      </Link>
-      <div className="flex flex-col gap-4 lg:w-3/4 lg:justify-between">
-        <div className="text-lg font-semibold">{item.title}</div>
-        <div className="flex justify-between">
-          <div className="flex items-center gap-2">
-            <img src="/pin.png" className="size-4" alt="marker-img" />
-            <span className="text-stone-500">{item.address}</span>
-          </div>
-          <span className="text-stone-800">
-            ({capitalizeWord(item.transaction)} /{" "}
-            {capitalizeWord(item.property)})
-          </span>
-        </div>
-        <div className="flex w-fit gap-3 font-semibold tracking-wide">
-          <div className="rounded-md border-2 border-yellow-400 px-4 py-2 text-yellow-400">
-            $ {item.price}
-          </div>
-          <Link
-            to={`/post/${item.id}`}
-            className="rounded-md bg-yellow-400 px-5 py-2 text-yellow-100 transition-all hover:bg-yellow-500"
-          >
-            View
-          </Link>
-        </div>
-        <div className="flex flex-col items-center justify-between gap-3 xs:flex-row xs:gap-0">
-          <div className="flex gap-4 lg:gap-2">
-            <div className="flex items-center gap-2 rounded-md bg-gray-100 px-2 py-1">
-              <img src="/bed.png" className="size-5 lg:w-4" alt="bed-img" />
-              <span className="text-xs">
-                {item.features.sizes.bedrooms} bedroom(s)
-              </span>
+    <li className="mx-1 my-1 w-full rounded-lg px-3 py-2 hover:outline hover:outline-1 hover:outline-yellow-400 lg:flex-row lg:gap-4">
+      <div className="flex flex-col gap-2 py-3 lg:flex-row lg:gap-4">
+        <Link
+          to={`/post/${item.id}`}
+          className="flex h-64 items-center lg:h-48 lg:w-64"
+        >
+          <img
+            src={item.image?.url.replace(
+              "upload/",
+              "upload/c_fill,h_260,w_750/",
+            )}
+            className="h-full w-full cursor-pointer rounded-md object-cover"
+            alt="house-img"
+          />
+        </Link>
+        <div className="flex flex-col gap-4 lg:w-3/4 lg:justify-between">
+          <div className="text-lg font-semibold">{item.title}</div>
+          <div className="flex flex-col justify-between gap-3 xs:flex-row">
+            <div className="flex items-center gap-2">
+              <img src="/pin.png" className="size-4" alt="marker-img" />
+              <span className="break-word text-stone-500">{item.address}</span>
             </div>
-            <div className="flex items-center gap-2 rounded-md bg-gray-100 px-2 py-1">
-              <img
-                src="/bath.png"
-                className="size-5 lg:w-4"
-                alt="bathtub-img"
-              />
-              <span className="text-xs">
-                {item.features.sizes.bathrooms} bathroom(s)
-              </span>
-            </div>
+            <span className="text-stone-800">
+              ({capitalizeWord(item.transaction)} /{" "}
+              {capitalizeWord(item.property)})
+            </span>
           </div>
-          <div className="flex gap-4 lg:gap-2">
-            <button
-              className="border border-stone-300 p-2 hover:border-stone-500 disabled:cursor-not-allowed"
-              onClick={() => handleSaveClick(item.id)}
-              disabled={isPending}
+          <div className="flex gap-3 font-semibold tracking-wide">
+            <div className="w-1/2 rounded-md border-2 border-yellow-400 px-4 py-2 text-center text-yellow-400 xs:w-fit">
+              $ {item.price}
+            </div>
+            <Link
+              to={`/post/${item.id}`}
+              className="flex w-1/2 items-center justify-center rounded-md bg-yellow-400 px-5 py-2 text-yellow-100 transition-all hover:bg-yellow-500 xs:w-fit"
             >
-              <img src="/save.png" className="size-4" alt="save-img" />
-            </button>
-            {user?.id !== item.user.id && (
+              View
+            </Link>
+          </div>
+          <div className="flex flex-col items-center justify-between gap-3 xs:flex-row xs:gap-0">
+            <div className="flex gap-4 lg:gap-2">
+              <div className="flex items-center gap-2 rounded-md bg-gray-100 px-2 py-1">
+                <img src="/bed.png" className="size-5 lg:w-4" alt="bed-img" />
+                <span className="text-xs">
+                  {item.features.sizes.bedrooms} bedroom(s)
+                </span>
+              </div>
+              <div className="flex items-center gap-2 rounded-md bg-gray-100 px-2 py-1">
+                <img
+                  src="/bath.png"
+                  className="size-5 lg:w-4"
+                  alt="bathtub-img"
+                />
+                <span className="text-xs">
+                  {item.features.sizes.bathrooms} bathroom(s)
+                </span>
+              </div>
+            </div>
+            <div className="flex gap-4 lg:gap-2">
               <button
                 className="border border-stone-300 p-2 hover:border-stone-500 disabled:cursor-not-allowed"
-                onClick={handleChatClick}
+                onClick={() => handleSaveClick(item.id)}
                 disabled={isPending}
               >
-                <img src="/chat.png" className="size-4" alt="chat-img" />
+                <img src="/save.png" className="size-4" alt="save-img" />
               </button>
-            )}
+              {user?.id !== item.user.id && (
+                <button
+                  className="border border-stone-300 p-2 hover:border-stone-500 disabled:cursor-not-allowed"
+                  onClick={handleChatClick}
+                  disabled={isPending}
+                >
+                  <img src="/chat.png" className="size-4" alt="chat-img" />
+                </button>
+              )}
+            </div>
           </div>
         </div>
       </div>
